b61b57abec4eae4af34d9977ce3ba334aa2d6326
HarryKing87 <babishh96@hotmail.com>
2020-10-01 20:35:57 +0300

    Arrow functions and this.

--- script.js
+++ script.js
@@ -470,3 +470,39 @@ const robot = {
 
 console.log(robot.provideInfo());
 
+// ARROW FUNCTIONS AND THIS.
+
+/*
+We saw in the previous exercise that for a method, the calling object is the object the method belongs to. If we use the this keyword in a method then the value of this is the calling object. However, it becomes a bit more complicated when we start using arrow functions for methods. Take a look at the example below:
+
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet: () => {
+    console.log(this.dietType);
+  }
+};
+
+goat.diet(); // Prints undefined
+In the comment, you can see that goat.diet() would log undefined. So what happened? Notice that in the .diet() is defined using an arrow function.
+
+Arrow functions inherently bind, or tie, an already defined this value to the function itself that is NOT the calling object. In the code snippet above, the value of this is the global object, or an object that exists in the global scope, which doesn’t have a dietType property and therefore returns undefined.
+
+To read more about either arrow functions or the global object check out the MDN documentation of the global object and arrow functions.
+
+The key takeaway from the example above is to avoid using arrow functions when using this in a method!
+
+Let's use the same example, but with a different
+name:
+*/
+
+const advancedRobot = {
+  energyLevel: 100,
+  checkEnergy()  {
+    console.log('Energy is currently at ' + advancedRobot.energyLevel + '%')
+  }
+}
+
+advancedRobot.checkEnergy();
