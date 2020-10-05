65554f9062b8a57a7f83bcf846eba03a311d93e7
HarryKing87 <babishh96@hotmail.com>
2020-10-05 10:59:51 +0300

    Added Team Stats Project

--- script.js
+++ script.js
@@ -839,3 +839,146 @@ console.log(robotEntries);
 const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true});
 
 console.log(newRobot);
+
+// TEAM STATS PROJECT
+
+/*
+We want to create and extract information about your favorite sports team.
+Basketball, soccer, tennis, or water polo, you pick it. 
+It’s your job to create data using the JavaScript data structures at your disposal: 
+arrays, objects, etc.
+
+Once created, you can manipulate these data structures as well 
+as gain insights from them. For example, you might want to get the 
+total number of games your team has played, or the average score of all 
+of their games.
+
+Steps:
+
+1.
+We want a data structure to store the information about our team. 
+Create an empty team object.
+
+2.
+Our team has players, and the team plays games. We want to represent both of these. 
+Add two properties to your team object. A _players property and a _games property 
+that are both initialized to empty arrays.
+
+3.
+Populate the empty array that corresponds to the _players key in your team 
+object with three actual players. They should be in the following format:
+
+{
+  firstName: 'Pablo',
+  lastName: 'Sanchez',
+  age: 11
+}
+You should put each player on a new line to prevent the line from getting too long.
+
+4.
+Populate the empty array that corresponds to the _games key in 
+your object with three actual games. They should be in the following format:
+
+{
+  opponent: 'Broncos',
+  teamPoints: 42,
+  opponentPoints: 27
+}
+
+5.
+Create getter methods for your _players and _games keys. 
+You do not need to create setter methods, because we don’t want 
+anyone to change the values saved to these keys.
+
+6.
+We want to add a new player to your team. 
+Add a method addPlayer to your team object. 
+This method should take in three parameters: firstName, 
+lastName, and age. It should create a player object, 
+and add them to the team‘s players array.
+
+7.
+Below your team object, invoke your addPlayer method on the following players:
+Steph Curry Age 28,Lisa Leslie Age 44, and Bugs Bunny Age 76.
+
+Print out the team‘s players to check they were all properly added.
+
+8.
+The scorekeeper has some new information for us! 
+Add a similar method for recording game results called addGame that:
+
+takes in an opponent’s name,
+your team’s points,
+the opponent’s points,
+creates a game object,
+adds the game object to your team‘s games array.
+
+9.
+Invoke your addGame method on three games and print the team‘s updated games array.
+
+The answer to the project is down below...
+*/
+
+const team = {
+  _players: [{
+    firstName: 'Harry',
+    lastName: 'King',
+    age: 23,
+    firstName2: 'John',
+    lastName2: 'Doe',
+    age2: 21,
+    firstName3: 'Bob',
+    lastName3: 'Ross',
+    age3: 22
+  }],
+  get players() {
+    return this._players;
+  },
+
+  _games: [{
+    opponent: 'Arrows',
+    teamPoints: 42,
+    opponentPoints: 40,
+    
+    opponent2: 'Eagles',
+    teamPoints2: 50,
+    opponentsPoints2:40,
+
+    opponent3: 'Bobcats',
+    teamPoints3: 49,
+    opponentPoints3: 39
+  }],
+  get games() {
+    return this._games;
+  },
+  addPlayer(firstName, lastName, age) {
+const player = {
+  firstName: firstName,
+  lastName: lastName,
+  age: age
+};
+this.players.push(player);
+  },
+
+  addGame(opponentName, teamPoints, opponentPoints) {
+const game = {
+  opponentName: opponentName,
+  teamPoints: teamPoints,
+  opponentPoints: opponentPoints
+};
+  this.games.push(game);
+}
+}
+
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team.players);
+
+team.addGame('Bucks', 82, 69);
+team.addGame('Knicks', 43, 52);
+team.addGame('Hawks', 83, 15);
+console.log(team.games);
+
+
+
