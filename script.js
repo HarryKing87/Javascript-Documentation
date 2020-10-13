3640dfcdc85070507fa08ee4b6c9f9e385a8e0cb
HarryKing87 <46583043+HarryKing87@users.noreply.github.com>
2020-10-13 14:31:39 +0300

    JSX Elements

--- script.js
+++ script.js
@@ -1732,4 +1732,30 @@ In this case, it means that JSX is not valid JavaScript. Web browsers can’t re
 If a JavaScript file contains JSX code, then that file will have to be compiled. That means that before the file reaches a web browser, a JSX compiler will translate any JSX into regular JavaScript.
 
 Codecademy’s servers already have a JSX compiler installed, so you don’t have to worry about that for now. Eventually we’ll walk through how to set up a JSX compiler on your personal computer.
+
+A basic unit of JSX is called a JSX element.
+
+Here’s an example of a JSX element:
+
+<h1>Hello world</h1>
+This JSX element looks exactly like HTML! The only noticeable difference is that you would find it in a JavaScript file, instead of in an HTML file.
+*/
+
+/*
+JSX elements are treated as JavaScript expressions. They can go anywhere that JavaScript expressions can go.
+
+That means that a JSX element can be saved in a variable, passed to a function, stored in an object or array…you name it.
+
+Here’s an example of a JSX element being saved in a variable:
+
+const navBar = <nav>I am a nav bar</nav>;
+Here’s an example of several JSX elements being stored in an object:
+
+const myTeam = {
+  center: <li>Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGuard: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billon</li>
+};
 */
