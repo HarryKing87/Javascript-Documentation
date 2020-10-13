b54256732f9e86c84bc87a21590c17eade574342
HarryKing87 <46583043+HarryKing87@users.noreply.github.com>
2020-10-13 13:26:39 +0300

    Deleted map() method

--- script.js
+++ script.js
@@ -702,31 +702,6 @@ fruits.forEach(printFruits);
 // I remind you that we can always do the same thing with a for loop, but in this example I'm showing you
 // the use of the functions and forEach method...
 
-// The .map() Method
-
-/*
-The second iterator we’re going to cover is .map(). When .map() is called on an array, it takes an argument of a callback function and returns a new array! Take a look at an example of calling .map():
-
-const numbers = [1, 2, 3, 4, 5]; 
-
-const bigNumbers = numbers.map(number => {
-  return number * 10;
-});
-.map() works in a similar manner to .forEach()— the major difference is that .map() returns a new array.
-
-In the example above:
-
-numbers is an array of numbers.
-bigNumbers will store the return value of calling .map() on numbers.
-numbers.map will iterate through each element in the numbers array and pass the element into the callback function.
-return number * 10 is the code we wish to execute upon each element in the array. This will save each value from the numbers array, multiplied by 10, to a new array.
-If we take a look at numbers and bigNumbers:
-
-console.log(numbers); // Output: [1, 2, 3, 4, 5]
-console.log(bigNumbers); // Output: [10, 20, 30, 40, 50]
-Notice that the elements in numbers were not altered and bigNumbers is a new array.
-*/
-
 
 
 
