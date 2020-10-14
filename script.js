42399959b07dbda9f49f73b91d2b253e6df688c1
HarryKing87 <46583043+HarryKing87@users.noreply.github.com>
2020-10-14 10:55:39 +0300

    Class Instance Example

--- script.js
+++ script.js
@@ -787,6 +787,38 @@ class Surgeon {
   }
 }
 
+/*
+Now, we’re ready to create class instances. An instance is an object that contains the property names and methods of a class, but with unique property values. Let’s look at our Dog class example.
+
+class Dog {
+  constructor(name) {
+    this.name = name;
+    this.behavior = 0;
+  } 
+}
+
+const halley = new Dog('Halley'); // Create new Dog instance
+console.log(halley.name); // Log the name value saved to halley
+// Output: 'Halley'
+Below our Dog class, we use the new keyword to create an instance of our Dog class. Let’s consider the line of code step-by-step.
+
+We create a new variable named halley that will store an instance of our Dog class.
+We use the new keyword to generate a new instance of the Dog class. The new keyword calls the constructor(), runs the code inside of it, and then returns the new instance.
+We pass the 'Halley' string to the Dog constructor, which sets the name property to 'Halley'.
+Finally, we log the value saved to the name key in our halley object, which logs 'Halley' to the console.
+*/
+
+class newSurgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new newSurgeon('Curry', 'Cardiovascular');
+
+const surgeonDurant = new newSurgeon('Durant', 'Orthopedics');
+
 
 
 
