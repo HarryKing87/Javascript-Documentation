b736cc1758d41fc7573575bd973059af4da98194
HarryKing87 <46583043+HarryKing87@users.noreply.github.com>
2020-10-15 10:13:04 +0300

    Method Calls Documentation

--- script.js
+++ script.js
@@ -898,6 +898,53 @@ const surgeonCurry = new brandNewSurgeon('Curry', 'Cardiovascular');
 const surgeonDurant = new brandNewSurgeon('Durant', 'Orthopedics');
 
 
+// METHOD CALLS
+
+/*
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+
+const halley = new Dog('Halley');
+In the example above, we create the Dog class, then create an instance, and save it to a variable named halley.
+
+The syntax for calling methods and getters on an instance is the same as calling them on an object — append the instance with a period, then the property or method name. For methods, you must also include opening and closing parentheses.
+
+Let’s take a moment to create two Dog instances and call our .incrementBehavior() method on one of them.
+
+let nikko = new Dog('Nikko'); // Create dog named Nikko
+nikko.incrementBehavior(); // Add 1 to nikko instance's behavior
+let bradford = new Dog('Bradford'); // Create dog name Bradford
+console.log(nikko.behavior); // Logs 1 to the console
+console.log(bradford.behavior); // Logs 0 to the console
+In the example above, we create two new Dog instances, nikko and bradford. Because we increment the behavior of our nikko instance, but not bradford, accessing nikko.behavior returns 1 and accessing bradford.behavior returns 0.
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
