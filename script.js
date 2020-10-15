21133686488a0e2e8078c150db7ed279b407b6ac
HarryKing87 <46583043+HarryKing87@users.noreply.github.com>
2020-10-15 10:12:12 +0300

    Methods

--- script.js
+++ script.js
@@ -842,9 +842,60 @@ const orthopedicsDep = new department('Orthopedics', 10);
 const pedologyDep = new department('Pedology', 8);
 
 
+// METHODS
 
+/*
+At this point, we have a Dog class that spins up objects with name and behavior properties. Below, we will add getters and a method to bring our class to life.
 
+Class method and getter syntax is the same as it is for objects except you can not include commas between methods.
 
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get behavior() {
+    return this._behavior;
+  }
+
+  incrementBehavior() {
+    this._behavior++;
+  }
+}
+In the example above, we add getter methods for name and behavior. Notice, we also prepended our property names with underscores (_name and _behavior), which indicate these properties should not be accessed directly. Under the getters, we add a method named .incrementBehavior(). When you call .incrementBehavior() on a Dog instance, it adds 1 to the _behavior property. Between each of our methods, we did not include commas.
+*/
+
+class brandNewSurgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+const surgeonCurry = new brandNewSurgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new brandNewSurgeon('Durant', 'Orthopedics');
 
 
 
